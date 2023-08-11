3899d67a34aaab3b215e8a458e814a7b7034cb13
Wachera <wacheram22@gmail.com>
2023-08-11 16:41:53 -0700

    for loop statements

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main-prints the leters of the alphabet
+ *
+ * Return:0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c >= 'z' ;	c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
