3f8112aaf105bde955c2dbebd26e40784699d32b
Wachera <wacheram22@gmail.com>
2023-08-16 16:53:30 -0700

    absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  _abs-determines absolute value of integer
+ *  @i:integer being checked
+ *Return:the absolute value
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (-i);
+	}
+
+
+}
