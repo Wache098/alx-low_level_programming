b88f97372f4ad8e428ef72e0f2f2683b36952c29
Wachera <wacheram22@gmail.com>
2023-08-16 02:35:28 -0700

    alphabet,

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,10 @@
 #include "main.h"
+#include <unistd.h>
 /**
  * print_alphabet-prints the alphabet, in lowercase, followed by a new line
  *
  * Return:0(success)
  */
-void print_alphabet(void);
-
-int main(void)
-{
-	print_alphabet();
-	_putchar('\n');
-	return (0);
-}
 void print_alphabet(void)
 {
 	char letter = 'a';
@@ -21,4 +14,5 @@ void print_alphabet(void)
 		_putchar(letter);
 		letter++;
 	}
+		_putchar('\n');
 	}
