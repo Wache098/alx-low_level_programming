9737f7b1c920cb6abf4581df888de9aa25f7bf19
Wachera <wacheram22@gmail.com>
2023-08-18 14:26:38 -0700

    print numbers except some

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_most_numbers-prints 0 to 9 except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			putchar(1 + '0');
+		}
+	}
+	putchar('\n');
+}
