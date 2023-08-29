fec2aa980f427aaef0fc0e0c074d0eb125c2113e
Wachera <wacheram22@gmail.com>
2023-08-28 18:13:48 -0700

    gets length of prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strspn-get length of a prefix substring
+ *
+ * @s:input string
+ *
+ * @accept:characters to search for
+ *
+ * Return:The number of bytes in the initial segment of s
+ * which consist only of bytes from accept.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned char charMap[256] = {0};
+	unsigned int count = 0;
+
+	while (*accept != '\0')
+	{
+		charMap[(unsigned char)(*accept)] = 1;
+		accept++;
+	}
+	while (charMap[(unsigned char)(*s)] != 0)
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
