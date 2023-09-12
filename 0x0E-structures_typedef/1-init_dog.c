91e703d41bd04c80de3cd44df733669784f03dd0
Wachera <wacheram22@gmail.com>
2023-09-12 10:01:55 -0700

    struct dog and initializing

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * init_dog -initializes a struct dog with provided values
+ * @d:pointer to the initialized struct dog
+ * @name:pointer to string name of dog
+ * @age:given age of dog
+ * @owner:pointer to string name of owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,7 @@
+ C - Structures, typedef
+0. Poppy
+ a new type struct dog with the following elements:
+
+name, type = char *
+age, type = float
+owner, type = char *
