3765cb2e7a08dfd5049ee460f7b82958a214c444
Wachera <wacheram22@gmail.com>
2023-09-12 10:15:28 -0700

    dog.h mainfile

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,7 +18,7 @@ struct dog
 /**
  * dog_a -typedef for struct dog
  */
-typedef struct dog dog_a;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
