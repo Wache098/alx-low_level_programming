39def9c4e2149f4c30781499681df4fa6d3e892a
Wache098 <wacheram22@gmail.com>
2023-09-26 11:07:25 -0700

    betty style correc

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,16 +10,16 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	 unsigned int count = 0;
+unsigned int count = 0;
 
-	 while (head != NULL)
-	 {
-		 if (count == index)
-		 {
-			 return (head);
-		 }
-		 head = head->next;
-		 count++;
-	 }
-	 return (NULL);
+while (head != NULL)
+{
+if (count == index)
+{
+return (head);
+}
+head = head->next;
+count++;
+}
+return (NULL);
 }
